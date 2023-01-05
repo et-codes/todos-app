0eb12c209b39742d9b80f131bfa7026ea63d2857
et-codes <104942211+et-codes@users.noreply.github.com>
2023-01-04 20:18:16 -0700

    Update TodoForm.

--- client-app/src/components/TodoForm.tsx
+++ client-app/src/components/TodoForm.tsx
@@ -1,3 +1,21 @@
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
 export default function TodoForm() {
-  return <div>Todo form goes here...</div>;
+  const [text, setText] = useState<string>("");
+  const [dueDate, setDueDate] = useState<Date | null>(null);
+
+  return (
+    <Form className="mb-3">
+      <Form.Group className="mb-1">
+        <Form.Label>Enter new task</Form.Label>
+        <Form.Control type="text" placeholder="Task description" />
+      </Form.Group>
+      <Form.Group className="mb-2">
+        <Form.Label>Due date (optional)</Form.Label>
+        <Form.Control type="date" />
+      </Form.Group>
+      <Button type="submit">Submit</Button>
+    </Form>
+  );
 }
